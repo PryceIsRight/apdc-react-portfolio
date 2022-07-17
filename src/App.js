6c4c1d12e622af9a751758cbdf609c557a9c9588
Aleczander Pryce Dela Cruz <105748265+PryceIsRight@users.noreply.github.com>
2022-07-16 20:46:00 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import moment from "moment";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 style={{ color: 'red', backgroundColor: 'black' }}>Aleczander Dela Cruz Portfolio</h1>
+      <div>
+        {moment().format('MMMM Do YYYY, h:mm:ss a')};
+      </div>
     </div>
   );
 }
